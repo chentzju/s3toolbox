3273168b422ab31673f1f9d2bb27d1ecb9ef957e
nfc-chent <chentao@zj.icbc.com.cn>
2016-05-24 18:24:52 +0800

    tools

--- src/S3ToolBox.js
+++ src/S3ToolBox.js
@@ -1,13 +1,43 @@
-!(function(){
-    "use strict"
+/*
+    definition of S3ToolBox
+ */
+"use strict";
+var S3ToolBox = (function(){
 
-    var S3ToolBox = function () {
+    var MagicBox =function(){
 
     };
 
-    S3ToolBox.prototype = {
+    MagicBox.prototype = {
 
     };
 
-    return S3ToolBox;
+    return MagicBox;
 })();
+
+/*
+    definition of Number Caculator
+ */
++function(S3){
+    var Caculator = function (){
+
+    };
+
+    Caculator.prototype = {
+
+    };
+
+    S3.Caculator = Caculator;
+}(S3ToolBox);
+
+/*
+    definition of other tools
+ */
++function(S3){
+
+    // your own tool definition
+
+
+    //add your tool to S3ToolBox
+    //S3.YourTool = YourTool;
+}(S3ToolBox);
