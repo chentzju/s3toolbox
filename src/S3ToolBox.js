9c083a839149c03f71bb1d36a9778073e9e55ae4
nfc-chent <chentao@zj.icbc.com.cn>
2016-07-21 10:50:51 +0800

    add utils and forms

--- src/S3ToolBox.js
+++ src/S3ToolBox.js
@@ -9,34 +9,17 @@ var S3 = (function ($) {
 
     var ToolBox = function(){};
 
-    return ToolBox;
+    return new ToolBox();
 })(jQuery);
-
-
 
 /*  .Utils.*/
 +function(toolBox){
-
-    // Keys in IE < 9 that won't be iterated by `for key in ...` and thus missed.
-    // IE < 9 is always the problem
-    var hasEnumBug = !{toString: null}.propertyIsEnumerable('toString');
-    function collectNonEnumProps(obj, keys) {
-        var nonEnumIdx = nonEnumerableProps.length;
-        var nonEnumerableProps = ['valueOf', 'isPrototypeOf', 'toString',
-            'propertyIsEnumerable', 'hasOwnProperty', 'toLocaleString'];
-        var constructor = obj.constructor;
-        var proto = (_.isFunction(constructor) && constructor.prototype) || ObjProto;
-
-        // Constructor is a special case.
-        var prop = 'constructor';
-        if (_.has(obj, prop) && !_.contains(keys, prop)) keys.push(prop);
-
-        while (nonEnumIdx--) {
-            prop = nonEnumerableProps[nonEnumIdx];
-            if (prop in obj && obj[prop] !== proto[prop] && !_.contains(keys, prop)) {
-                keys.push(prop);
-            }
-        }
+    /**
+     *兼容浏览器把-替换为/
+     *@param val
+     **/
+    function delimiterConvert(val){
+        return val.replace(/-/g,'/');
     }
 
     /**
@@ -45,17 +28,51 @@ var S3 = (function ($) {
      */
     var Utils = {
 
+        /**
+         * 判断是否数组
+         */
         isArray : Array.isArray || function(obj) {
             return toString.call(obj) === '[object Array]';
         },
 
+        /**
+         * 判断是否数字
+         * @param obj
+         * @returns {boolean}
+         */
         isNumber : function(obj){
             return typeof obj === 'number';
         },
 
+        /**
+         * 判断是否对象
+         * @param obj
+         * @returns {boolean}
+         */
         isObject : function(obj) {
             var type = typeof obj;
             return type === 'function' || type === 'object' && !!obj;
+        },
+        /**
+         *
+         */
+        isPlainObject:function(obj){
+            var proto,ctor,o={};
+            if(!obj || toString.call(obj) !== "[object Object]"){
+                return false;
+            }
+            proto = Object.getPrototypeOf(obj);
+
+            if(!proto){
+                return true;
+            }
+
+
+            var hasOwn = o.hasOwnProperty;
+            var fn2String = hasOwn.toString;
+
+            ctor = o.hasOwnProperty.call(proto,"constructor") && proto.constructor;
+            return typeof ctor === "function" && fn2String.call(ctor) === fn2String.call(Object)
         },
 
         // Is a given value equal to null?
@@ -68,20 +85,36 @@ var S3 = (function ($) {
             return obj === void 0;
         },
 
+        /**
+         * 判断某对象是否含有某属性
+         * @param obj
+         * @param key
+         * @returns {boolean}
+         */
         has : function(obj, key) {
             return obj != null && Object.prototype.hasOwnProperty.call(obj, key);
         },
 
+        /**
+         * 返回对象的属性数组
+         * @param obj
+         * @returns {Array}
+         */
         keys : function(obj) {
             if (!Utils.isObject(obj)) return [];
             if (Object.keys) return Object.keys(obj);
             var keys = [];
             for (var key in obj) if (Utils.has(obj, key)) keys.push(key);
             // Ahem, IE < 9.
-            if (hasEnumBug) collectNonEnumProps(obj, keys);
+            //if (hasEnumBug) collectNonEnumProps(obj, keys);
             return keys;
         },
 
+        /**
+         * 返回对象的值数组
+         * @param obj
+         * @returns {*}
+         */
         values : function(obj) {
             var keys = Utils.keys(obj);
             var length = keys.length;
@@ -94,19 +127,137 @@ var S3 = (function ($) {
 
         clone : function(obj) {
              if (!Utils.isObject(obj)) return obj;
-             return _.isArray(obj) ? obj.slice() : _.extend({}, obj);
+             return Utils.isArray(obj) ? obj.slice() : Utils.extend({}, obj);
+        },
+        extend:function(obj){
+
+        },
+
+        /**
+         * 日期比较
+         * @param date1
+         * @param date2
+         * @returns {boolean}
+         */
+        dateCompare:function(date1,date2){
+            var date1=new Date(delimiterConvert(date1)).getTime()
+            var date2=new Date(delimiterConvert(date2)).getTime();
+            if (date1>date2) {
+                return true;
+            }else{
+                return false;
+            }
         }
-    };
+};
 
     //
-    toolBox.Utils = Utils;
+    toolBox.utils = Utils;
+}(S3);
+
+/**
+ * Forms
+ */
++function (toolBox) {
+    var Forms = function () {
+
+    };
+
+    Forms.prototype={
+
+
+        /**
+         * 将form表单中的数据封装成JSON对象。
+         * @param form
+         * @return obj
+         */
+        form2json:function(form){
+            var obj = {};
+            var a = $(form).serializeArray();
+            $.each(a,function(){
+                if(obj[this.name]!==undefined){
+                    if(!obj[this.name].push){
+                        obj[this.name]=[obj[this.name]];
+                    }
+                    obj[this.name].push(this.value||'');
+                }else{
+                    obj[this.name]=this.value||'';
+                }
+            });
+            return obj;
+        },
+        /**
+         * 将json数据填入form中。
+         * @param form,jsonObj,pre
+         */
+
+        json2form:function(form,jsonObj,pre){
+        var key,value,name,pre,eles,match;
+
+        pre = pre || '';
+
+        //遍历对象
+        for(key in jsonObj){
+            name = pre + key;
+            value = jsonObj[key];
+
+            //如果还是对象，那就要递归  基本用不到
+            if(toolBox.utils.isPlainObject(value)){
+                json2form(form,value,key+'.');
+            }else{
+                //查找form中 名字与name匹配的元素
+                eles = [];
+                match = form.elements;
+                for(var i = 0 ;i<match.length;i++){
+                    if(match[i].getAttribute('name') === name){
+                        eles.push(match[i]);
+                    }
+                }
+                //返回的匹配都是数组，直接计算即可
+                eles.forEach(function(elex){
+                    //select
+                    if(elex.tagName.toLowerCase() === "select") {
+                        if(elex.type === "select-multiple"){
+                            for(var i = 0;i<elex.options.length;i++){
+                                value.forEach(function (x) {
+                                    if(elex.options[i].value === x)
+                                        elex.options[i].selected = true;
+                                });
+                            }
+                        }else{
+                            for (var i = 0; i < elex.options.length; i++) {
+                                if (elex.options[i].value === value) {
+                                    elex.selectedIndex = i;
+                                }
+                            }
+                        }
+                    }
+                    //checkbox
+                    else if(elex.type === "checkbox"){
+                        elex.checked = (elex.value === value || value.some(function(x){ return elex.value === x})) ? true:false;
+                    }
+                    // radio
+                    else if(elex.type === "radio") {
+                        elex.checked = elex.value === value ? true :false;
+                    }else{
+                        elex.value = value;
+                    }
+                });
+            }
+        }
+    }
+    };
+
+    toolBox.form = new Forms();
 }(S3);
 
 /**
  *
  */
 +function(toolBox){
-    var str = '<div class = "preloader">loading</div>';
+
+    //var eventStart = touchstart || touchstart;
+
+    var str = '<div class = "preloader">正在加载中</div>';
 
     var totalheight=0;
     var callback = null;
@@ -142,6 +293,7 @@ var S3 = (function ($) {
     }
     toolBox.ScrollLoad = ScrollLoad;
 }(S3);
+
 
 /*
     Number Calculator
@@ -240,7 +392,12 @@ var S3 = (function ($) {
             }
         };
 
-        //数组加数组
+        /**
+         * 数组加上数组
+         * @param arr1
+         * @param arr2
+         * @returns {Array}
+         */
         var addArr = function (arr1,arr2){
             var newArr=[];
             if (arr1.length===arr2.length) {
@@ -254,7 +411,12 @@ var S3 = (function ($) {
         };
 
 
-        //数组减数组
+        /**
+         * 数组减去数组
+         * @param arr1
+         * @param arr2
+         * @returns {Array}
+         */
         var subArr = function (arr1,arr2){
             var newArr=[];
             if (a.length===b.length) {
@@ -350,6 +512,9 @@ var S3 = (function ($) {
             return value === undefined ? value:value.replace(/\,/g,'');
         };
 
+
+
+
         return {
             //四则运算
             add: add,
@@ -373,5 +538,5 @@ var S3 = (function ($) {
 
     })();
 
-    toolBox.Cal = Cal;
+    toolBox.cal = Cal;
 }(S3);
